dba388cca614ae5749c6abbac7af7f204039a4d6
Behzad Haghgoo <behzad.haghgoo@gmail.com>
2021-12-13 20:48:48 -0800

    remove options

--- pkg/brew/tym.rb
+++ pkg/brew/tym.rb
@@ -14,7 +14,7 @@ class Tym < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install "cli.sh"
     bin.install "install.sh"
