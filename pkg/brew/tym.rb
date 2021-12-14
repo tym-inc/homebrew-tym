4bf2ee9db69dd4b9fcc8b028d62baf1ea180c656
Behzad Haghgoo <behzad.haghgoo@gmail.com>
2021-12-13 16:15:07 -0800

    new brew

--- pkg/brew/tym.rb
+++ pkg/brew/tym.rb
@@ -1,17 +1,44 @@
-class TymBin < Formula
-  version '0.0.1'
-  desc "Cli for Tym Editor"
-  homepage "https://github.com/tym-inc/homebrew-tym"
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewTym < Formula
+  desc "Instant Code Collaboration"
+  homepage "https://tym.so"
+  url "https://github.com/tym-inc/homebrew-tym/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "55bbaf6d84ee6b49d19eab0a43456cbacd8f0743e3eed19c916f20ba2e1c4086"
+  license ""
 
-  if OS.linux?
-    url ""
-    sha256 ""
-  elsif OS.mac?
-    url "https://github.com/tym-inc/homebrew-tym/releases/download/0.0.2/tym.tar.gz"
-    sha256 "db238757c2a2cb2dd82390f565c8f3168c8839bef911b8b11c3b16c305a1ed59"
-  end
+  # depends_on "cmake" => :build
 
   def install
-    bin.install "tym"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "cli.sh"
+    bin.install "install.sh"
+    bin.install "package.json"
+    bin.install "tym_install.sh"
+    bin.install "node" 
+    bin.install "product.json"
+    bin.install Dir["node_modules"]
+    bin.install Dir["extensions"]
+    bin.install Dir["out"]
+
+    
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-tym`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
